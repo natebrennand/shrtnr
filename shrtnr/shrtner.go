68ac3d3398046f8209b27c4d9020c89154372d54
Nate Brennand <natebrennand@gmail.com>
2013-12-21 00:41:35 -0800

    init and map out shrtnr methods

---
+++ shrtnr/shrtner.go
@@ -0,0 +1,17 @@
+package shrtnr
+
+// returns a randomly generated shortened URL
+func shortenURL(url string) (string, error) {
+	// should generate the code then call createURL()
+	return "", nil
+}
+
+// creates the requested shortened URL
+func createURL(logURL string, shortURL string) (string, error) {
+	return "", nil
+}
+
+// retrieves a URL based on the shortened URL
+func retrieveURL(shortURL string) (string, error) {
+	return "", nil
+}
